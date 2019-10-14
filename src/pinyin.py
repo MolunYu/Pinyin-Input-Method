484607ad816f49b17e84d3a189c4e2313af50001
MolunYu <molunyu97@gmail.com>
2019-10-14 10:02:37 +0800

    version 1.1

--- src/pinyin.py
+++ src/pinyin.py
@@ -2,11 +2,21 @@ from viterbi import viterbi
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename", type=str)
+parser.add_argument("--input", type=str, required=True, help="input file path")
+parser.add_argument("--output", type=str, help="output file path")
 args = parser.parse_args()
 
-with open(args.filename, mode="r") as source:
+with open(args.input, mode="r") as source:
+    result = list()
     for line in source.readlines():
-        print(viterbi(line[:-1].split(" ")))
+        sentence = viterbi(line[:-1].split(" "))
+        print(sentence)
+        result.append(sentence + "\n")
+
+    if args.output:
+        with open("output.txt", mode="w") as target:
+            target.writelines(result)
+
+
 
 
